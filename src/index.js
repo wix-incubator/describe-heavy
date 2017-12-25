c04bc56a8dd4fbde9b1a216156bb76ca4758da2c
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-25 09:24:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,29 @@ describe.heavy = (name, fn) =>
       fn();
     }
   });
+
+function describeHeavy(desc, fn) {
+  if (process.env.WIX_NODE_BUILD_WATCH_MODE) {
+    describe.skip.apply(this, arguments);
+  } else {
+    describe(desc, function () {
+      if (this.timeout) {
+        this.timeout(0);
+      }
+      fn();
+    });
+  }
+}
+
+function itHeavy() {
+  if (process.env.WIX_NODE_BUILD_WATCH_MODE) {
+    it.skip.apply(this, arguments);
+  } else {
+    const result = it.apply(this, arguments);
+    if (result.timeout) {
+      result.timeout(0);
+    }
+  }
+}
+
+module.exports = {describeHeavy, itHeavy};
